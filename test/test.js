6093d538a737d0d24d5fb4dbd2c1ff154795089d
anzerr <ryan.anzer@epitech.eu>
2019-03-26 16:33:43 +0100

    dump

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 
-const Fork = require('./index.js'),
+const Fork = require('../index.js'),
 	path = require('path');
 
 const base = require('path').resolve(__dirname).replace(/\\/g, '/');
 
-let f = new Fork(path.join(base, 'src/fork.js'), [
+let f = new Fork(path.join(base, 'fork.js'), [
 	{key: '1', threshold: 0xfffffc0000000000},
 	{key: '2', threshold: 0xfffffc0000000000}
 ]);
